c2172b062d7c32359cb1a30d710f2b2622c55f5a
Locolm <clement.kl34@gmail.com>
2023-06-07 17:58:13 +0200

    classe météo

--- src/View.java
+++ src/View.java
@@ -14,6 +14,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JButton;
+import java.util.Random;
 
 public class View extends JFrame implements Observer {
 
